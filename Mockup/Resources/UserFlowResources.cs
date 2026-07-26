a19c3d23970387a79f627d4a2ebe4ad05cfd82b2
Claus <FalkensteinVIA@gmx.de>
2026-07-26 11:15:38 +0200

    feat: add runtime localization foundation

---
+++ Mockup/Resources/UserFlowResources.cs
@@ -0,0 +1,15 @@
+using System.Resources;
+
+namespace Mockup.Resources;
+
+/// <summary>
+/// Provides the application-owned strings used by the UserFlow user interface.
+/// </summary>
+public static class UserFlowResources
+{
+    private static readonly ResourceManager ResourceManagerInstance = new(
+        "Mockup.Resources.UserFlowStrings",
+        typeof(UserFlowResources).Assembly);
+
+    public static ResourceManager ResourceManager => ResourceManagerInstance;
+}
